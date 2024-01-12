bf2c2b2920c29af7e73a05f4aedb845169fd7c71
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-12 18:18:59 +0530

    add user image

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -3,10 +3,18 @@ import React from "react";
 import "./UserItem.css";
 export default function UserItem({ user }) {
   return (
-    <li key={user.id}>
-      {user.name}
-      {user.places}
-      <img src={user.image} alt={user.name} />
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={user.image} alt={user.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{user.name}</h2>
+          <h3>
+            {user.places} {user.places === 1 ? "Place" : "Places"}
+          </h3>
+        </div>
+      </div>
     </li>
   );
 }
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,13 +1,12 @@
 import React from "react";
 import UsersList from "../components/UsersList";
-
+import profile from "../../Assets/Images/smalldp.png";
 const Users = () => {
   const USERS = [
     {
       id: "u1",
       name: "Vrushabh",
-      image:
-        "https://images.unsplash.com/photo-1612833603929-5b7b8e5b7b8e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80",
+      image: profile,
       places: 3,
     },
   ];
