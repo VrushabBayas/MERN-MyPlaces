1bf46fa0ffd838e40f7559acb629c8a40d190f1b
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-12 18:40:31 +0530

    add style foer userItm add common Avatar and Card components

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,20 +1,27 @@
 import React from "react";
 
 import "./UserItem.css";
+import Avatar from "../../shared/UIElements/Avatar/Avatar/Avatar";
+import { Link } from "react-router-dom";
+import Card from "../../shared/UIElements/Avatar/Card/Card";
 export default function UserItem({ user }) {
   return (
     <li className="user-item">
-      <div className="user-item__content">
-        <div className="user-item__image">
-          <img src={user.image} alt={user.name} />
-        </div>
-        <div className="user-item__info">
-          <h2>{user.name}</h2>
-          <h3>
-            {user.places} {user.places === 1 ? "Place" : "Places"}
-          </h3>
-        </div>
-      </div>
+      <Card className="user-item__content">
+        <Link to={`/${user.id}/places`}>
+          <div className="user-item__content">
+            <div className="user-item__image">
+              <Avatar image={user.image} alt={user.name} />
+            </div>
+            <div className="user-item__info">
+              <h2>{user.name}</h2>
+              <h3>
+                {user.places} {user.places === 1 ? "Place" : "Places"}
+              </h3>
+            </div>
+          </div>
+        </Link>
+      </Card>
     </li>
   );
 }
