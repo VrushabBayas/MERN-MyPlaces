55973a1b6c9b70ead7e6154566ffa95f259b81eb
Vrushab Bayas <31801565+VrushabBayas@users.noreply.github.com>
2024-01-15 08:50:32 +0530

    add side drawer

--- src/shared/Navigation/SideDrawer.js
+++ src/shared/Navigation/SideDrawer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./SideDrawer.css";
 function SideDrawer(props) {
   return <aside className="side-drawer">{props.children}</aside>;
 }
